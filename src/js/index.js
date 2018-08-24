25c75d8853a3888c3157db6dd96cdc36c7d14670
Will <wfoster12@gmail.com>
2018-08-24 17:20:25 +0100

    Recipe view completed. Format units remaining

--- src/js/index.js
+++ src/js/index.js
@@ -2,6 +2,7 @@
 import Search from './models/Search';
 import Recipe from './models/Recipe';
 import * as searchView from './views/searchView';
+import * as recipeView from './views/recipeView';
 import {elements, renderLoader, clearLoader} from './views/base';
 
 /* Global state of the app
@@ -48,7 +49,6 @@ elements.searchForm.addEventListener('submit', e => {
 })
 
 
-
 elements.searchResPages.addEventListener('click', e =>{
     const btn = e.target.closest('.btn-inline');
     if(btn){
@@ -66,6 +66,9 @@ const controlRecipe = async () => {
 
     if(id){
         //prepare UI for changes
+        recipeView.clearRecipe();
+        renderLoader(elements.recipe);
+
         try{
             //Create new recipe object
             state.recipe = new Recipe(id);
@@ -76,7 +79,8 @@ const controlRecipe = async () => {
             state.recipe.calcTime();
             state.recipe.calcServings();
             // Render recipe
-            console.log(state.recipe);
+            clearLoader();
+            recipeView.renderRecipe(state.recipe);
         } catch(err){
             alert('Error processing recipe')
         }
